ec1297ac0ef27d3ad92a2a5e6db03d3e07d2b02e
SNSantos <36704867+SNSantos@users.noreply.github.com>
2022-05-30 18:57:05 +0200

    Add files via upload

--- Vehicule.c
+++ Vehicule.c
@@ -1,5 +1,175 @@
-//
-// Created by basti on 24.05.2022.
-//
+/*
+  ---------------------------------------------------------------------------
+  Fichier     :
+  Nom du labo :
+  Auteur(s)   : Bastien Pillonel
+  Date        :
+  But         : le but du programme et non le but du laboratoire !!
+
+  Remarque(s) : à compléter
+
+  Compilateur : MingW-w64 g++ 11.2.0
+  ---------------------------------------------------------------------------
+*/
 
 #include "Vehicule.h"
+#include <stdio.h>
+#include <inttypes.h>
+#include <math.h>
+
+const char* const TYPES_VEHICULE[] 		= {"Voiture", "Camionnette"};
+const char* const GAMME_VEHICULE[] 		= {"Standard", "Haut de gamme"};
+const char* const CARACTERISTIQUES[]	= {"Marque", "Matricule",
+														"Categorie", "Gamme",
+														"Poids", "Cylindree",
+														"Rejet CO2", "Puissance",
+														"Volume de transport",
+														"Taxe annuelle"};
+
+const double 	TAXE_BASE_CAMIONNETTE 	= 700.,
+					TAXE_BASE_VOITURE			= 400.,
+					TAXE_VOITURE_ECO   		= 0.,
+					TAXE_VOITURE_POL			= 50.,
+					TAUX_VOITURE_GROSSE_CYL = 0.05,
+					TAUX_CAMIONNETTE			= 10.,
+					TAXE_VOITURE_HG			= 200.,
+					TAXE_VOITURE_HG_PUISSANT= 300,
+					TAUX_VOITURE_HG_PUISSANT= 20.;
+
+const uint16_t SEUIL_PUISSANCE	= 250,
+					SEUIL_CYLINDREE	= 1400,
+					SEUIL_REJET_CO2	= 130;
+
+Vehicule voitureStandard(Matricule matricule, Marque marque, uint16_t poids,
+                         uint16_t cylindre, uint16_t rejetCo2) {
+
+   Vehicule vhc = {  .matricule = matricule,
+                     .marque = marque,
+                     .typeVehicule = VOITURE,
+                     .categorie = {.voiture = {.poids = poids,
+                                               .gammeVehicule = STANDARD,
+                                               .gamme = {.standard = {.cylindre =
+                                                                      cylindre,
+                                                                      .rejetCo2 =
+                                                                         rejetCo2
+                                               }}}}
+   };
+
+   return vhc;
+
+}
+
+Vehicule voitureHautGamme(Matricule matricule, Marque marque, uint16_t poids,
+                          uint16_t puissance) {
+
+   Vehicule vhc = {  .matricule = matricule,
+                     .marque = marque,
+                     .typeVehicule = VOITURE,
+                     .categorie = {.voiture = {.poids = poids,
+                                               .gammeVehicule = HAUT_GAMME,
+                                               .gamme = {.hautGamme = {.puissance
+                                                                       = puissance
+                                               }}}}
+   };
+
+   return vhc;
+}
+
+Vehicule camionnette(Matricule matricule, Marque marque, double volumeTransport) {
+
+   Vehicule vhc = {  .matricule = matricule,
+      .marque = marque,
+      .typeVehicule = CAMIONNETTE,
+      .categorie = {.camionnette = {.volumeTransport = volumeTransport
+                                    }}
+   };
+
+   return vhc;
+}
+
+void affichage(const Vehicule *vehicule){
+   printf("=================================================\n"
+			 "%-" ESPACEMENT "s" ": %s\n"
+			 "%-" ESPACEMENT "s" ": %s\n"
+			 "%-" ESPACEMENT "s" ": %s\n",
+          CARACTERISTIQUES[0], vehicule->marque,
+			 CARACTERISTIQUES[1], vehicule->matricule,
+			 CARACTERISTIQUES[2], TYPES_VEHICULE[vehicule->typeVehicule]);
+
+   switch (vehicule->typeVehicule) {
+      case VOITURE:
+         printf("%-" ESPACEMENT "s" ": %s\n"
+				"%-" ESPACEMENT "s" ": %" PRIu16 POIDS_UNITEE "\n",
+				CARACTERISTIQUES[3],
+				GAMME_VEHICULE[vehicule->categorie.voiture.gammeVehicule],
+				CARACTERISTIQUES[4], vehicule->categorie.voiture.poids);
+
+         switch (vehicule->categorie.voiture.gammeVehicule) {
+            case STANDARD:
+               printf("%-" ESPACEMENT "s" ": %" PRIu16 CYLINDRE_UNITEE "\n"
+							 "%-" ESPACEMENT "s" ": %" PRIu16 REJET_CO2_UNITEE "\n",
+						CARACTERISTIQUES[5],
+                  vehicule->categorie.voiture.gamme.standard.cylindre,
+						CARACTERISTIQUES[6],
+                  vehicule->categorie.voiture.gamme.standard.rejetCo2);
+               break;
+
+            case HAUT_GAMME:
+               printf("%-" ESPACEMENT "s" ": %" PRIu16 PUISSANCE_UNITEE "\n",
+							 CARACTERISTIQUES[7],
+                      vehicule->categorie.voiture.gamme.hautGamme.puissance);
+               break;
+         }
+         break;
+
+      case CAMIONNETTE:
+         printf("%-" ESPACEMENT "s" ": %.2f" VOLUME_UNITEE "\n",
+					 CARACTERISTIQUES[8],
+                vehicule->categorie.camionnette.volumeTransport);
+         break;
+   }
+   printf("%-" ESPACEMENT "s" ": %g " DEVISE "\n"
+		"=================================================\n",
+		CARACTERISTIQUES[9], taxe(vehicule));
+}
+
+double taxe(const Vehicule *vehicule){
+	double taxe;
+
+	switch (vehicule->typeVehicule) {
+		case VOITURE:
+			switch (vehicule->categorie.voiture.gammeVehicule) {
+				case STANDARD:
+					if(vehicule->categorie.voiture.gamme.standard.cylindre < SEUIL_CYLINDREE)
+						if(vehicule->categorie.voiture.gamme.standard.rejetCo2 < SEUIL_REJET_CO2)
+							taxe = TAXE_BASE_VOITURE + TAXE_VOITURE_ECO;
+						else
+							taxe = TAXE_BASE_VOITURE + TAXE_VOITURE_POL;
+					else
+						taxe = TAXE_BASE_VOITURE +
+						TAUX_VOITURE_GROSSE_CYL * vehicule->categorie.voiture.gamme.standard.cylindre;
+					break;
+
+				case HAUT_GAMME:
+					if(vehicule->categorie.voiture.gamme.hautGamme.puissance <= SEUIL_PUISSANCE)
+						taxe = TAXE_BASE_VOITURE + TAXE_VOITURE_HG;
+					else
+						taxe = TAXE_BASE_VOITURE + TAXE_VOITURE_HG_PUISSANT +
+						TAUX_VOITURE_HG_PUISSANT *	vehicule->categorie.voiture.poids /
+						1000.;
+					break;
+			}
+			break;
+
+		case CAMIONNETTE:
+			taxe = vehicule->categorie.camionnette.volumeTransport * TAUX_CAMIONNETTE
+			+ TAXE_BASE_CAMIONNETTE;
+			break;
+	}
+
+	return arrondis5Centimes(taxe);
+}
+
+double arrondis5Centimes(double montant){
+	return round(montant * 20) / 20.0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,61 @@
-#include <stdio.h>
+/*
+  ---------------------------------------------------------------------------
+  Fichier     :
+  Nom du labo :
+  Auteur(s)   : Bastien Pillonel
+  Date        :
+  But         : le but du programme et non le but du laboratoire !!
 
-int main() {
-   printf("Hello, World!\n");
+  Remarque(s) : à compléter
+
+  Compilateur : MingW-w64 g++ 11.2.0
+  ---------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Vehicule.h"
+#include "Statistique.h"
+
+int compare(const void* vhc1, const void* vhc2);
+
+int main(void) {
+
+   //Vehicule voiture = {.matricule = "123456", .marque = "Peugeot"};
+   //printf("%s\n%s", voiture.matricule, voiture.marque);
+
+   // Test constructeur
+   Vehicule vs1 = voitureStandard("1234567","Toyota", 1287, 1320,119);
+	Vehicule vs2 = voitureStandard("1654894","Mazda", 987, 1290,130);
+	Vehicule vs3 = voitureStandard("1568531","Volkswagen", 1879, 1540,129);
+
+   Vehicule vh1 = voitureHautGamme("1856482","Porsche", 1287, 250);
+	Vehicule vh2 = voitureHautGamme("4582131","BMW", 1500, 500);
+
+   Vehicule c1 = camionnette("6542184","Iveco", 10.567);
+	Vehicule c2 = camionnette("3846854","ISUZU", 5.625);
+
+	Vehicule* parking[] = {&vs1, &vs2, &vs3, &vh1, &vh2, &c1, &c2};
+
+	const size_t TAILLE_PARKING = sizeof(parking) / sizeof(Vehicule*);
+
+	printf("=============== AVANT QSORT ===============\n");
+
+	for(size_t i = 0; i < TAILLE_PARKING; ++i){
+		affichage(parking[i]);
+	}
+
+	qsort(parking, TAILLE_PARKING, sizeof(Vehicule*), compare);
+
+	printf("=============== APRES QSORT ===============\n");
+
+	for(size_t i = 0; i < TAILLE_PARKING; ++i){
+		affichage(parking[i]);
+	}
+
    return 0;
+}
+
+int compare(const void* vhc1, const void* vhc2){
+	return (int)(taxe(*(Vehicule**)vhc2) - taxe(*(Vehicule**) vhc1));
 }
